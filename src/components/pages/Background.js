10c06d0556192b71262b93f1c537927eb72b4f86
Shauna Saunders <shaunacsaunders@outlook.com>
2021-12-30 15:43:10 -0500

    Add content to background page

--- src/components/pages/Background.js
+++ src/components/pages/Background.js
@@ -11,18 +11,35 @@ function Background() {
                     <div className="col-6">
                         <p className="p-content">
                             A child destined for STEM in a country that expected otherwise. <br></br>
-                            Growing up, I always excelled in mathematics and loved putting things together/taking things apart.
-                            I just loved understanding how things work. <br></br>
-                            Growing up, I would take my electronics apart because
-                            I was SO intriuged by how everything worked and HAD to know what 
-                            was inside.<br></br>
-                            My favorite subject was math. 
-                            I loved logic and problem-solving. <br></br>
-                            I wasn't aware of it then, but a career in STEM was the ideal 
-                            career path for me.
                         </p>
                         <p className="p-content">
-                            
+                            As a kid, I constantly got into trouble for taking my toys and electronics apart.
+                            I was just SO intrigued by how everything worked and HAD to know what was inside.
+                            I would get so excited when my parents brought home a new piece of furniture
+                            that needed assembling.<br></br>
+                            In school, my favorite subject was math. I loved logic and problem-solving.
+                        </p>
+                        <p className="p-content">
+                            I wasn't aware of it then, but a career in STEM was the ideal career path for me.
+                        </p>
+                        <p className="p-content">
+                                Going into high school, I was the only person of color in my class.
+                                I also <i>just so happen</i> to be one of the only people in my class not
+                                recommended to take higher-level math courses despite my great academic record.
+                                It took weeks of finishing assignments with significant time to spare before I was transferred
+                                into a more challenging course.
+                        </p>
+                        <p className="p-content">
+                                Going into college, I wanted to pursue engineering, but I doubted myself and my capabilities.
+                                I operated with the belief that it wasn't a field for people that looked like me.
+                        </p>
+                        <p className="p-content">
+                                It may have taken longer than it should have, but I made it to the field I thrive in.
+                        </p>
+                        <p className="p-content">
+                            I continue to thrive and share my stories, so children of color can see what they're capable of...
+                            So they can experience the relief of having an interviewer that looks like them... So they can define
+                            their own future...
                         </p>
                     </div>
                     <div className="col-6">
