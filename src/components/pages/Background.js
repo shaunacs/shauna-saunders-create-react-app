ec300edd595bbcd74e5a26f42f9f2ecb7257f186
Shauna Saunders <shaunacsaunders@outlook.com>
2021-12-30 14:36:48 -0500

    Add content to background page

--- src/components/pages/Background.js
+++ src/components/pages/Background.js
@@ -9,11 +9,10 @@ function Background() {
             <div className="page-text">
                 <div className="row">
                     <div className="col-6">
-                        <h2 className="p-heading">
-                                Great question.
-                        </h2> 
                         <p className="p-content">
-                            I've always loved understanding how things work. <br></br>
+                            A child destined for STEM in a country that expected otherwise. <br></br>
+                            Growing up, I always excelled in mathematics and loved putting things together/taking things apart.
+                            I just loved understanding how things work. <br></br>
                             Growing up, I would take my electronics apart because
                             I was SO intriuged by how everything worked and HAD to know what 
                             was inside.<br></br>
