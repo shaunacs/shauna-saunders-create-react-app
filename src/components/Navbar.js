cd7934cee24a98b447eb55bbfc861e1b4282f9fc
Shauna Saunders <shaunacsaunders@outlook.com>
2021-12-31 04:24:14 -0500

    Change site logo to S

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,8 @@ function Navbar() {
             <nav className="navbar">
                 <div className="navbar-container">
                     <Link to="/" className="navbar-logo" onClick={closeMobileMenu}>
-                        <i class="fas fa-laptop-code"></i> SAUNDERS
+                        <img id="navbar-img" src="images/s-logo.png"></img>
+                        {/* <i class="fas fa-laptop-code"></i> SAUNDERS */}
                     </Link>
                     <div className="menu-icon" onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
