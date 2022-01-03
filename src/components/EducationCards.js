09df01091dd52388577d855f6f5eaadc7a8f69d6
Shauna Saunders <shaunacsaunders@outlook.com>
2022-01-03 09:39:36 -0500

    Add UNC to Education cards

--- src/components/EducationCards.js
+++ src/components/EducationCards.js
@@ -13,6 +13,11 @@ function EducationCards() {
                         src="https://coursereport-s3-production.global.ssl.fastly.net/uploads/school/logo/9/original/new-logo-monochromatic-gradient.png"
                         school="Hackbright Academy"
                         studies="Software Engineering Fellowship"/>
+                        <CardLink 
+                        url="https://ed.unc.edu/"
+                        src="https://upload.wikimedia.org/wikipedia/en/thumb/5/5c/University_of_North_Carolina_at_Chapel_Hill_seal.svg/1200px-University_of_North_Carolina_at_Chapel_Hill_seal.svg.png"
+                        school="The University of North Carolina at Chapel Hill"
+                        studies="Human Development &amp; Family Studies"/>
                     </ul>
                 </div>
             </div>
